1599d41a15966decf62fbb141cb30b78ec0e0981
lizreznik <525ereznik@frhsd.com>
2024-09-12 11:25:40 -0400

    website started

---
+++ script.js
@@ -0,0 +1,19 @@
+function studentLife() {
+    document.getElementById('response-header').innerText = 'Student Life'
+    document.getElementById('response-text').innerText = '...'
+}
+
+function academicPrograms() {
+    document.getElementById('response-header').innerText = 'Academic Programs'
+    document.getElementById('response-text').innerText = '...'
+}
+
+function costFinance() { 
+    document.getElementById('response-header').innerText = 'Cost & Financial Aid'
+    document.getElementById('response-text').innerText = '...'
+}
+
+function alumniSuccess() { 
+    document.getElementById('response-header').innerText = 'Alumni Success'
+    document.getElementById('response-text').innerText = '...'
+}
