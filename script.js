911186ceb07014002697eb81b3d6a91a0e317517
lizreznik <144464805+lizreznik@users.noreply.github.com>
2024-09-23 01:29:55 +0000

    academic programs done

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function studentLife() {
 
 function academicPrograms() {
     document.getElementById('response-header').innerText = 'Academic Programs'
-    document.getElementById('response-text').innerText = '...'
+    document.getElementById('response-text').innerText = 'Montclair State University offers a diverse range of academic programs, encompassing over 300 majors, minors, concentrations, and certificate programs. The university is known for its strong programs in fields such as business, education, health sciences, and the arts. With a student-to-teacher ratio of 17 to 1, students at Montclair State benefit from personalized attention and a supportive learning environment. Here are a few of the majors offered within the university: College of the Arts (CART): Animation and Visual Effects, Art History, Dance, Music | Theatre Arts College for Education & Engaged Learning (CEEL): Early Childhood Education, Elementary Education, Secondary Education, Special Education, Sport Management |College for Community Health (CCHL): Nursing, Public Health, Health Administration | College of Humanities & Social Sciences (CHSS, Anthropology, English, History, Philosophy, Psychology, Sociology | College of Science & Mathematics (CSAM): Biology, Chemistry, Computer Science, Mathematics | Physics, Feliciano School of Business (FSOB): Accounting, Finance, Marketing, Management, Economic'
 }
 
 function costFinance() { 
